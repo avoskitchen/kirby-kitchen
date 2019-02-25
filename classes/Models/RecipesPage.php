4752452b0a79ea1158c6405d55d0266a21bd4285
Fabian Michael <hallo@fabianmichael.de>
2019-02-25 23:07:08 +0100

    Add a method to retrieve the lastest recipes.

--- classes/Models/RecipesPage.php
+++ classes/Models/RecipesPage.php
@@ -19,4 +19,31 @@ class RecipesPage extends Page
         $this->setProperties($props);
         $this->hasPrivateItems = option('avoskitchen.kitchen.privateRecipes');
     }
+
+    public function latest(int $limit = 0, bool $unlisted = false, $private = false): Collection {
+        
+        $items = $this->children();
+        
+        if ($unlisted === false) {
+            $items = $items->listed();
+        }
+
+        if ($this->hasPrivateItems() && $private === true) {
+            // Filter out private items, if user is not logged-in
+            $user = $this->kirby()->user();
+            if ($user === null) {
+                $items = $items->filter(function ($item) {
+                    return $item->isPrivate() === false;
+                });
+            }
+        }
+
+        $items = $items->sortBy('created', 'desc');
+
+        if ($limit > 0) {
+            $items = $items->limit($limit);
+        }
+
+        return $items;
+    }
 }
