e238e95b11abe618b82f338000366935ddef9711
Fabian Michael <hallo@fabianmichael.de>
2020-04-26 17:48:17 +0200

    Fix click handler

--- src/js/components/ingredients.js
+++ src/js/components/ingredients.js
@@ -1,20 +1,19 @@
-import ready from '../utils/ready';
+import ready from "../utils/ready";
 
 class Ingredients {
   constructor(el) {
     this.el = el;
 
-    this.el.addEventListener('click', function(e) {
-      const el = e.target.closest('.ingredient');
-      el !== null && el.classList.toggle('is-complete');
+    this.el.addEventListener("click", function(e) {
+      const el = e.target.closest(".ingredient");
+      el !== null && el.classList.toggle("is-complete");
     });
   }
 }
 
 
 ready(() => {
-
-  const ingredients = document.querySelectorAll('.js-kitchen-ingredients');
+  const ingredients = document.querySelectorAll(".js-kitchen-ingredients");
 
   for (let i = 0, l = ingredients.length; i < l; i++) {
     new Ingredients(ingredients[i]);
