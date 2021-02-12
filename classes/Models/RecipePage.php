f344f79ec715a9bdedd21eab81e9a6a2242c628a
Fabian Michael <hallo@fabianmichael.de>
2021-02-12 11:48:06 +0100

    Add text output option to yield method.

--- classes/Models/RecipePage.php
+++ classes/Models/RecipePage.php
@@ -8,6 +8,7 @@ use AvosKitchen\Kitchen\Traits\HasCategoryField;
 use Kirby\Cms\Field;
 use Kirby\Cms\Page;
 use Kirby\Cms\Collection;
+use Kirby\Toolkit\Str;
 
 class RecipePage extends Page
 {
@@ -100,7 +101,7 @@ class RecipePage extends Page
         return $yieldFactor;
     }
 
-    public function yieldFormatted(): string
+    public function yieldFormatted(string $output = 'html'): string
     {
         if ($this->yield()->isEmpty()) {
             return '';
@@ -121,11 +122,17 @@ class RecipePage extends Page
             $unit = '';
         }
 
-        return snippet('yield', [
+        $data = [
             'yield' => $currentYield,
             'unit' => $unit,
             'isDefaultYield' => $this->currentYield() !== $this->defaultYield(),
-        ], true);
+        ];
+
+        if ($output === 'text') {
+            return Str::template('{{ yield }} {{ unit }}', $data);
+        } else {
+            return snippet('yield', $data, true);
+        }
     }
 
     public function cuisinesFormatted()
